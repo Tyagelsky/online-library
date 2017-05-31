db7328c479230bc40cd559674dd3b6ffc57d32b8
Yaroslav <tyagelsky@yandex.ru>
2017-06-01 02:49:30 +0300

    add category controller and model, add book_category model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ActiveRecord::Base
   has_attached_file :avatar, styles: { thumb: "100x100>", medium: "200x200>" },  :default_url => "missing_:style.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  validates :name, presence: true, length: {minimum: 1, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10, maximum: 450}
+
+  has_many :book_categories
+  has_many :categories, through: :book_categories
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531213033) do
+ActiveRecord::Schema.define(version: 20170531234038) do
+
+  create_table "book_categories", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +29,12 @@ ActiveRecord::Schema.define(version: 20170531213033) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
